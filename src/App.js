302a74e456bfab50a362ccf977c0e8c5d3ffdd66
Camilo Caetano <camilo.s.caetano@gmail.com>
2020-05-06 10:35:06 -0300

    Hello React

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const sayHello = ()=>{
+
+      alert("hello world ");
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello React 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={sayHello} >Say Hello</button>
       </header>
     </div>
   );
